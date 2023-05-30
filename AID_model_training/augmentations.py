651df46b6a8ec57373c91ab92c356192f7264c92
Marco Bazzani <mbazzani@ucsd.edu>
2023-05-29 17:43:02 -0700

    Initial naive augmentations

---
+++ AID_model_training/augmentations.py
@@ -0,0 +1,31 @@
+import numpy as np
+import librosa
+
+def add_noise(data, noise_factor):
+    noise = np.random.randn(len(data))
+    augmented_data = data + noise_factor * noise
+    # Cast back to same data type
+    augmented_data = augmented_data.astype(type(data[0]))
+    return augmented_data
+
+def time_shift(data, sampling_rate, shift_max, shift_direction):
+    shift = np.random.randint(sampling_rate * shift_max)
+    if shift_direction == 'right':
+        shift = -shift
+    elif shift_direction == 'both':
+        direction = np.random.randint(0, 2)
+        if direction == 1:
+            shift = -shift
+    augmented_data = np.roll(data, shift)
+    # Set to silence for heading/ tailing
+    if shift > 0:
+        augmented_data[:shift] = 0
+    else:
+        augmented_data[shift:] = 0
+    return augmented_data
+
+def pitch_shift(data, sampling_rate, pitch_factor):
+    return librosa.effects.pitch_shift(data, sampling_rate, pitch_factor)
+
+def speed_shift(data, speed_factor):
+    return librosa.effects.time_stretch(data, speed_factor)
